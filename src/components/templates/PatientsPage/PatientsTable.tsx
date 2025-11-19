14f9ce6efa48c6a9c5e6cb3219a28999a1ba6edb
winchester-cell <manili.mm91@gmail.com>
2025-11-19 22:38:36 +0330

    patient table responsive

--- src/components/templates/PatientsPage/PatientsTable.tsx
+++ src/components/templates/PatientsPage/PatientsTable.tsx
@@ -4,9 +4,9 @@ import PatientTableRow from "./TableItem/PatientTableRow"
 const PatientsTable = () => {
     return (
         <div className="w-full p-2 neu__norm rounded-3xl scale-[1]">
-            <div className="w-full p-3 neu__inner rounded-2xl flex flex-col gap-3">
+            <div className="w-full p-3 neu__inner rounded-2xl flex flex-col gap-3 overflow-x-auto">
                 {/* Table head */}
-                <div className="text-[clamp(4px,1dvw,12px)] lg:text-[12px] py-3 px-4 lg:px-7 lg:py-5 grid grid-flow-col auto-cols-fr neu__norm rounded-2xl font-semibold text-white bg-(--hover-color)">
+                <div className="w-4xl xl:w-full text-[12px] px-7 py-5 grid grid-flow-col auto-cols-fr neu__norm rounded-2xl font-semibold text-white bg-(--hover-color)">
                     <div className="border-e-2 border-white ps-5">Name</div>
                     <div className="border-e-2 border-white ps-5">Patient Code</div>
                     <div className="border-e-2 border-white ps-5">Phone</div>
--- src/components/templates/PatientsPage/TableItem/PatientTableRow.tsx
+++ src/components/templates/PatientsPage/TableItem/PatientTableRow.tsx
@@ -2,7 +2,7 @@ import { MdModeEdit } from "react-icons/md"
 
 const PatientTableRow = ({ name, patientCode, phone, gender, sessions, lastVisit }: any) => {
     return (
-        <div className="text-[clamp(4px,1dvw,12px)] lg:text-[12px] py-3 px-4 lg:px-7 lg:py-5 grid grid-flow-col auto-cols-fr neu__norm rounded-2xl">
+        <div className="w-4xl xl:w-full text-[12px] px-7 py-5 grid grid-flow-col auto-cols-fr neu__norm rounded-2xl">
             <div className="border-e-2 border-(--main-text) ps-5">{name}</div>
             <div className="border-e-2 border-(--main-text) ps-5">{patientCode}</div>
             <div className="border-e-2 border-(--main-text) ps-5">{phone}</div>
@@ -10,7 +10,7 @@ const PatientTableRow = ({ name, patientCode, phone, gender, sessions, lastVisit
             <div className="border-e-2 border-(--main-text) ps-5">{sessions}</div>
             <div className="border-e-2 border-(--main-text) ps-5">{lastVisit}</div>
             <div className="ps-5">
-                <MdModeEdit className="lg:w-5 lg:h-5 w-2 h-2" />
+                <MdModeEdit className="w-5 h-5" />
             </div>
         </div>
     )
